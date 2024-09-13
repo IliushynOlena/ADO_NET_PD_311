25f8e5b408e86f9029154f116e61300d83e46a5b
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-09-13 17:14:33 +0300

    Data access library

---
+++ WPF_Client/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using _02_CRUD_Interface;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string connectionString;
+        SportShopDb db = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["SportShopDbConnection"]
+                .ConnectionString;
+            db = new SportShopDb(connectionString);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.GetAll();
+        }
+    }
+}
---
+++ data_access/SportShopDb.cs
@@ -0,0 +1,142 @@
+﻿using System.Data.SqlClient;
+
+namespace _02_CRUD_Interface
+{
+    public class SportShopDb
+    {
+        //CRUD Interface
+        //[C]reate
+        //[R]ead
+        //[U]pdate
+        //[D]elete
+        string name = "Olena";
+        private SqlConnection conn;
+        private string connectionString;
+        public SportShopDb(string connectionString)
+        {     
+            this.connectionString = connectionString;
+            conn = new SqlConnection(connectionString);
+            conn.Open();    
+        }
+        ~SportShopDb()
+        {
+            conn.Close();   
+        }
+        public void Create(Product product)
+        {
+            string cmdText = $@"INSERT INTO Products
+                              VALUES (@name, 
+                                      @type, 
+                                      @Quantity, 
+                                      @Price, 
+                                      @Producer, 
+                                      @CostPrice)";
+
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            command.Parameters.AddWithValue("name", product.Name);
+            command.Parameters.AddWithValue("type", product.Type);
+            command.Parameters.AddWithValue("Quantity", product.Quantity);
+            command.Parameters.AddWithValue("Price", product.Price);
+            command.Parameters.AddWithValue("Producer", product.Producer);
+            command.Parameters.AddWithValue("CostPrice", product.CostPrice);
+            command.CommandTimeout = 5; // default - 30sec
+            
+            //// ExecuteNonQuery - виконує команду яка не повертає результат 
+            ///(insert, update, delete...),
+            ////але метод повертає кількітсь рядків, які були задіяні
+            int rows = command.ExecuteNonQuery();
+
+            Console.WriteLine(rows + " rows affected!");
+        }
+        public List<Product> GetAll()
+        {
+            string cmdText = @"select * from Products";
+
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            SqlDataReader reader = command.ExecuteReader();
+            return this.GetProductsByQuery(reader);
+        }
+        public List<Product> GetByName(string name)
+        {
+           
+            //Stanga';drop table Hack;--
+            string cmdText = $@"select * from Products where Name = @name";
+
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            //command.Parameters.Add("name",System.Data.SqlDbType.NVarChar).Value = name;
+            SqlParameter parameter = new SqlParameter()
+            {
+                ParameterName = "name",
+                SqlDbType = System.Data.SqlDbType.NVarChar,
+                Value = name
+            };
+            command.Parameters.Add(parameter);
+            SqlDataReader reader = command.ExecuteReader();
+            return this.GetProductsByQuery(reader);
+        }
+        public Product GetById(int id)
+        {
+            string cmdText = $@"select * from Products where Id = {id}";
+
+            SqlCommand command = new SqlCommand(cmdText, conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            return this.GetProductsByQuery(reader).FirstOrDefault()!;
+
+        }
+        private List<Product> GetProductsByQuery(SqlDataReader reader)
+        {
+
+            List<Product> products = new List<Product>();
+            ////// відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+                products.Add(new Product()
+                {
+                    Id = (int)reader[0],
+                    Name = (string)reader[1],
+                    Type = (string)reader[2],
+                    Quantity = (int)reader[3],
+                    CostPrice = (int)reader[4],
+                    Producer = (string)reader[5],
+                    Price = (int)reader[6]
+                });
+            }
+
+            reader.Close();
+            return products;
+
+        }
+        public void Update(Product product)
+        {
+            string cmdText = $@"UPDATE Products
+                              SET Name =@name, 
+                                  TypeProduct =@type, 
+                                  Quantity =@Quantity, 
+                                  CostPrice =@CostPrice, 
+                                  Producer =@Producer, 
+                                  Price =@Price
+                                  where Id = {product.Id}";
+
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            command.Parameters.AddWithValue("name", product.Name);
+            command.Parameters.AddWithValue("type", product.Type);
+            command.Parameters.AddWithValue("Quantity", product.Quantity);
+            command.Parameters.AddWithValue("Price", product.Price);
+            command.Parameters.AddWithValue("Producer", product.Producer);
+            command.Parameters.AddWithValue("CostPrice", product.CostPrice);
+            command.CommandTimeout = 5; // default - 30sec
+
+            command.ExecuteNonQuery();
+
+        }
+        public void Delete(int id)
+        {
+            string cmdText = $@"delete Products where Id = {id}";
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
